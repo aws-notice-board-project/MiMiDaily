74b51b6de2f46d86cae685bb2633c8f3f656cd04
pado8 <floe9235@gmail.com>
2025-03-31 17:44:35 +0900

    add:ArticlesDAO ArticlesDTO 추가

---
+++ mimidaily/src/main/java/com/mimidaily/dto/ArticlesDTO.java
@@ -0,0 +1,62 @@
+package com.mimidaily.dto;
+
+public class ArticlesDTO {
+    private int idx;
+    private String title;
+    private String content;
+    private int category;
+    private String created_at;
+    private int visitcnt;
+    
+    public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public String getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+	public int getVisitcnt() {
+		return visitcnt;
+	}
+	public void setVisitcnt(int visitcnt) {
+		this.visitcnt = visitcnt;
+	}
+	public String getMembers_id() {
+		return members_id;
+	}
+	public void setMembers_id(String members_id) {
+		this.members_id = members_id;
+	}
+	public int getThumnails_idx() {
+		return thumnails_idx;
+	}
+	public void setThumnails_idx(int thumnails_idx) {
+		this.thumnails_idx = thumnails_idx;
+	}
+	private String members_id;
+    private int thumnails_idx;
+
+}
