9440816846fd49fedb6eebe4975ef0452567821d
syundo <syundo@outlook.kr>
2025-04-02 17:17:50 +0900

    eidt: 아이디 중복 체크 기능

--- mimidaily/src/main/java/com/mimidaily/controller/JoinServlet.java
+++ mimidaily/src/main/java/com/mimidaily/controller/JoinServlet.java
@@ -32,16 +32,32 @@ public class JoinServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "member/join.jsp"; 
-		String type;
-		if ("reporter".equals(request.getParameter("role"))) {
-		    type = "reporter";
+		String url = "member/join.jsp";
+		// 기자인지 일반 회원인지 구분
+//		String type;
+//		if ("reporter".equals(request.getParameter("role"))) {
+//		    type = "reporter";
+//		} else {
+//		    type = "user";
+//		}
+//		request.setAttribute("job", type);
+//		System.out.println(request.getParameter("role"));
+//		System.out.println(type);
+		
+		// id 중복 체크 전달(id, error_msg)
+		String id = request.getParameter("id");
+		String ide;
+		MemberDAO mDao = new MemberDAO();
+		int idOk = mDao.confirmID(id);
+		if(idOk == 1) {
+			ide = "사용 불가능한 아이디 입니다.";
 		} else {
-		    type = "user";
+			ide = "사용 가능한 아이디 입니다.";
 		}
-		request.setAttribute("job", type);
-		System.out.println(request.getParameter("role"));
-		System.out.println(type);
+		request.setAttribute("id_error", ide);
+		
+		// 기자 인증 코드 확인?
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 		dispatcher.forward(request, response);//주소가 변경되지 않음.
 	}
@@ -50,32 +66,29 @@ public class JoinServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "member/login.jsp";
+		String url = "member/join.jsp";
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pw");
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
 		String tel = request.getParameter("tel");
-//		String birth = request.getParameter("birth");
-//		String gender = request.getParameter("gender");
 		MemberDTO mDto = new MemberDTO();
 		mDto.setId(id);
 		mDto.setPwd(pwd);
 		mDto.setName(name);
 		mDto.setEmail(email);
 		mDto.setTel(tel);
-//		mDto.setAdmin(Integer.parseInt(admin));
-//		MemberDAO mDao = MemberDAO.getInstance();
-//		int result = mDao.insertMember(mDto);
-//		HttpSession session = request.getSession();
-//		if (result == 1) {
-//			session.setAttribute("id", mDto.getUserid());
-//			request.setAttribute("message", "회원 가입에 성공했습니다.");
-//			RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-//		} else {
-//			request.setAttribute("message", "회원 가입에 실패했습니다.");
-//		}
-//		dispatcher.forward(request, response);
+		MemberDAO mDao = new MemberDAO();
+		int result = mDao.insertMember(mDto);
+		HttpSession session = request.getSession();
+		if (result == 1) {
+			session.setAttribute("id", mDto.getId());
+			request.setAttribute("success_msg", "회원 가입에 성공했습니다.");
+			url = "member/login.jsp";
+		} else {
+			request.setAttribute("success_msg", "회원 가입에 실패했습니다.");
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
 	}
-
 }
