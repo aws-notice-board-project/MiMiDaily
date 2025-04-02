c95ea068981c7db64545638fce23e9b77af470fb
seasign10 <seasign10@gmail.com>
2025-04-02 11:44:02 +0900

    edit: memberDAO type

--- mimidaily/src/main/java/com/mimidaily/dto/MemberDTO.java
+++ mimidaily/src/main/java/com/mimidaily/dto/MemberDTO.java
@@ -9,7 +9,7 @@ public class MemberDTO {
     private String birth;
     private String gender;
     private Boolean marketing;
-    private String role;
+    private int role;
     private Integer profile_idx;
     private int visitcnt;
     private String created_at;
@@ -62,25 +62,20 @@ public class MemberDTO {
 	public void setMarketing(Boolean marketing) {
 		this.marketing = marketing;
 	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
-
 	public int getRole() {
 		return role;
 	}
 	public void setRole(int role) {
 		this.role = role;
 	}
+
 	public int getVisitcnt() {
 		return visitcnt;
 	}
 	public void setVisitcnt(int visitcnt) {
 		this.visitcnt = visitcnt;
-
+	}
+	
 	public Integer getProfile_idx() {
 		return profile_idx;
 	}
