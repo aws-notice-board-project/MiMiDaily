06c2b7354c25b847f03359f0a7f999706f2e4c40
syundo <syundo@outlook.kr>
2025-04-10 13:53:41 +0900

    fix: 주민번호 오류 수정

--- mimidaily/src/main/java/com/mimidaily/controller/member/JoinServlet.java
+++ mimidaily/src/main/java/com/mimidaily/controller/member/JoinServlet.java
@@ -42,14 +42,13 @@ public class JoinServlet extends HttpServlet {
 		    MemberDAO mDao = new MemberDAO();
 		    int idOk = mDao.confirmID(id);
 		    mDao.close();
-
+		    
 		    String ide;
 		    if (idOk == 1) {
 		        ide = "사용 불가능한 아이디입니다. 다른 아이디로 변경해 주세요.";
 		    } else {
 		        ide = "사용 가능한 아이디입니다.";
 		    }
-
 		    response.setContentType("text/plain; charset=UTF-8");
 		    response.getWriter().write(ide);
 		    return; // 여기서 끝냄
@@ -60,18 +59,27 @@ public class JoinServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8"); // 한글 깨짐을 방지
 		String url = "member/join.jsp";
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pw");
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
 		String tel = request.getParameter("tel");
+		String birth = request.getParameter("birth");
+		String gender = request.getParameter("gender");
+		int role = Integer.parseInt(request.getParameter("role"));
+		Boolean marketing = "1".equals(request.getParameter("marketing"));
 		MemberDTO mDto = new MemberDTO();
 		mDto.setId(id);
 		mDto.setPwd(pwd);
 		mDto.setName(name);
 		mDto.setEmail(email);
 		mDto.setTel(tel);
+		mDto.setBirth(birth);
+		mDto.setGender(gender);
+		mDto.setRole(role);
+		mDto.setMarketing(marketing);
 		MemberDAO mDao = new MemberDAO();
 		int result = mDao.insertMember(mDto);
 		HttpSession session = request.getSession();
