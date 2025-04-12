8c4696c5b70a264e90ff17da2315c050ee9091ca
syundo <syundo@outlook.kr>
2025-04-12 14:58:53 +0900

    edit: Member.DTO 수정(프로필 관련 파트 추가)

--- mimidaily/src/main/java/com/mimidaily/dto/MemberDTO.java
+++ mimidaily/src/main/java/com/mimidaily/dto/MemberDTO.java
@@ -14,6 +14,12 @@ public class MemberDTO {
     private int visitcnt;
     private String created_at;
     
+    private String ofile;
+	private String sfile;
+	private String file_path;
+	private long file_size;
+	private String file_type;
+    
 	public String getId() {
 		return id;
 	}
@@ -85,5 +91,37 @@ public class MemberDTO {
 	}
 	public void setCreated_at(String created_at) {
 		this.created_at = created_at;
+	}
+	
+	// 프로필 getter setter 추가
+    public String getOfile() {
+		return ofile;
+	}
+	public void setOfile(String ofile) {
+		this.ofile = ofile;
+	}
+	public String getSfile() {
+		return sfile;
+	}
+	public void setSfile(String sfile) {
+		this.sfile = sfile;
+	}
+	public String getFile_path() {
+		return file_path;
+	}
+	public void setFile_path(String file_path) {
+		this.file_path = file_path;
+	}
+	public long getFile_size() {
+		return file_size;
+	}
+	public void setFile_size(long fileSize) {
+		this.file_size = fileSize;
+	}
+	public String getFile_type() {
+		return file_type;
+	}
+	public void setFile_type(String file_type) {
+		this.file_type = file_type;
 	}
 }
