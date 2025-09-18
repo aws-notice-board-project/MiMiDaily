3f7be6bd12b3ded511ffc035e3f5364e95e10e7b
Dongsusin <auroratime020715@gmail.com>
2025-09-18 10:18:42 +0900

    git action

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -5,8 +5,8 @@
 
 AWS_REGION="ap-northeast-2"            # ⚠️ 본인 리전
 ACCOUNT_ID="${AWS_ACCOUNT_ID}"         # EC2 환경변수 또는 하드코딩
-REPO_NGINX="nginx"                     # ⚠️ ECR nginx repository
-REPO_TOMCAT="tomcat"                   # ⚠️ ECR tomcat repository
+REPO_NGINX="ecr_nginx"                     # ⚠️ ECR nginx repository
+REPO_TOMCAT="ecr_tomcat"                   # ⚠️ ECR tomcat repository
 PORT_NGINX="80"                        # ⚠️ HostPort (nginx)
 PORT_TOMCAT="8080"                     # ⚠️ HostPort (tomcat)
 
