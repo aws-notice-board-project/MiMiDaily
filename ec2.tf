5a2605e87be03eb13dbec878b55efcd0453c90ca
Dongsusin <auroratime020715@gmail.com>
2025-09-22 11:31:54 +0900

    terraform change test

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,16 @@
+# provider.tf
 provider "aws" {
   region = "ap-northeast-2"
 }
 
+# ec2.tf
 resource "aws_instance" "ec2" {
-  ami = "ami-0a5a6128e65676ebb"
+  ami           = "ami-0a5a6128e65676ebb" # Amazon Linux 2 (ap-northeast-2)
   instance_type = "t2.micro"
 }
 
+# outputs.tf
 output "ec2_ip" {
-    value = aws_instance.ec2.private_ip
+  description = "Private IP of the EC2 instance"
+  value       = aws_instance.ec2.private_ip
 }
